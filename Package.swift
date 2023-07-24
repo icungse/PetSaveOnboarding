2a1aaf55a6ae05ee1a2870092ee649913bd634be
Icung <saiful-islam@outlook.co.id>
2023-07-24 17:08:37 +0700

    initial commit

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PetSaveOnboarding",
+    platforms: [.iOS(.v15), .macOS(.v10_15)],
+    products: [
+        .library(
+            name: "PetSaveOnboarding",
+            targets: ["PetSaveOnboarding"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "PetSaveOnboarding",
+            resources: [.copy("Resources/Assets.xctassets")]
+        )
+    ]
+)
